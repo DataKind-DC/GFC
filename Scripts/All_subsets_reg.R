b84ef5da57cacb9c6cba0f2f799e676e6086f259
Matthew Barclay <matthew.barclay@me.com>
2019-02-02 10:41:13 -0500

    completed activity data

--- Scripts/All_subsets_reg.R
+++ Scripts/All_subsets_reg.R
@@ -27,9 +27,9 @@ names(MyData)
 
 ## Dropped from consideration for now: "Awards Data" (requires join), "Grant use" (NLP complexity) 
 
-OCI_subset <- MyData[c(1:14,29,49,50,51,52,110:117)]
+OCI_subset <- MyData[c(1:14,20,29,49,50,51,52,110:117)]
 
-
+names(OCI_subset)
 
 ## Import and manipulate the activity data, make sure the filepath is correct
 
@@ -38,7 +38,7 @@ MyData2 <- read.csv(file="~/R_Studio_Projects/GFC/New Data/Activity_Flat.csv",he
 names(MyData2)
 
 ## Need to total number of activities per year per organization
-## I'm most comfortable doing that in SQL with nested where clauses
+## I'm most comfortable doing that in SQL with Groupbys
 ## Need to rename some columns first though:
 
 colnames(MyData2)[colnames(MyData2)=="Org.Name"] <- "Org_Name"
@@ -51,31 +51,36 @@ names(MyData2)
 library(sqldf)
 library(pastecs)
 
+
+##Need to convert "done date" to fiscal year by shifting by 184 days (from jan 1 to june 30)
 MyData2$Done.Dateform <- as.Date(MyData2$Done.Date,format = "%m/%d/%Y")
 names(MyData2)
 head(MyData2)
-
 MyData2$Fiscal_Year <- format(MyData2$Done.Dateform  + 184,"%Y")
 head(MyData2)
 
 
 
 
-## Get site visits
-head(sqldf("SELECT Org_Name , Fiscal_Year , Count(*) AS 'Site_Visit' FROM MyData2 WHERE Capacity_Building_Type = 'Site Visit' GROUP BY Org_Name, Activity_Year"))
 
 
-##Create first column
-ActivityData <- sqldf("SELECT Org_Name , Fiscal_Year , Count(*) AS 'Site_Visit' FROM MyData2 WHERE Capacity_Building_Type = 'Site Visit' GROUP BY Org_Name, Fiscal_Year")
+##Create list of orgs and fiscal years
+
+ActivityData <- sqldf("SELECT Org_Name , Fiscal_Year FROM MyData2 GROUP BY Org_Name, Fiscal_Year")
 head(ActivityData)
+##Create site visit column
+ActivityData <- sqldf("SELECT a.Org_Name, a.Fiscal_Year, b.Site_Visit from ActivityData AS a LEFT JOIN 
+                      (SELECT Org_Name, Fiscal_Year, Count(*) AS 'Site_Visit' FROM MyData2 WHERE Capacity_Building_Type = 'Site Visit' GROUP BY Org_Name, Fiscal_Year) AS b
+                      ON  b.Org_Name = a.Org_Name  AND b.Fiscal_Year = a.Fiscal_Year")
 ##Create Kowledge Exchange Column
 ActivityData <- sqldf("SELECT a.Org_Name, a.Fiscal_Year, a.Site_Visit, b.Knowledge_Exchange from ActivityData AS a LEFT JOIN 
                       (SELECT Org_Name, Fiscal_Year, Count(*) AS 'Knowledge_Exchange' FROM MyData2 WHERE Capacity_Building_Type = 'Knowledge Exchange Participation' GROUP BY Org_Name, Fiscal_Year) AS b
                       ON  b.Org_Name = a.Org_Name  AND b.Fiscal_Year = a.Fiscal_Year")
 ##Create Leveraging Column
-ActivityData <- sqldf("SELECT a.Org_Name, a.Fiscal_Year, a.Site_Visit, a.Knowledge_Exchange , b.Leveraging,  from ActivityData AS a LEFT JOIN 
+ActivityData <- sqldf("SELECT a.Org_Name, a.Fiscal_Year, a.Site_Visit, a.Knowledge_Exchange , b.Leveraging  from ActivityData AS a LEFT JOIN 
                       (SELECT Org_Name, Fiscal_Year, Count(*) AS 'Leveraging' FROM MyData2 WHERE Capacity_Building_Type = 'Leveraging' GROUP BY Org_Name, Fiscal_Year) AS b
                       ON  b.Org_Name = a.Org_Name  AND b.Fiscal_Year = a.Fiscal_Year")
+
 ##Total Leveraging amount for year - represented in thousands
 ActivityData <- sqldf("SELECT a.Org_Name, a.Fiscal_Year, a.Site_Visit, a.Knowledge_Exchange , a.Leveraging, b.Leveraging_Amount  from ActivityData AS a LEFT JOIN 
                       (SELECT Org_Name, Fiscal_Year, Sum(Leveraging_Amount) AS 'Leveraging_Amount' FROM MyData2 WHERE Capacity_Building_Type = 'Leveraging' GROUP BY Org_Name, Fiscal_Year) AS b
@@ -89,17 +94,33 @@ ActivityData <- sqldf("SELECT a.Org_Name, a.Fiscal_Year, a.Site_Visit, a.Knowled
 ActivityData <- sqldf("SELECT a.Org_Name, a.Fiscal_Year, a.Site_Visit, a.Knowledge_Exchange , a.Leveraging, a.Leveraging_Amount, a.Phone_Call, b.Email from ActivityData AS a LEFT JOIN 
                       (SELECT Org_Name, Fiscal_Year, count(*) AS 'Email' FROM MyData2 WHERE Capacity_Building_Type = 'E-mail' GROUP BY Org_Name, Fiscal_Year) AS b
                       ON  b.Org_Name = a.Org_Name  AND b.Fiscal_Year = a.Fiscal_Year")
-
+##Create "aditional" column
 ActivityData <- sqldf("SELECT a.Org_Name, a.Fiscal_Year, a.Site_Visit, a.Knowledge_Exchange , a.Leveraging, a.Leveraging_Amount, a.Phone_Call, a.Email, b.Additional from ActivityData AS a LEFT JOIN 
                       (SELECT Org_Name, Fiscal_Year, count(*) AS 'Additional' FROM MyData2 WHERE Capacity_Building_Type = 'Additional Touch' GROUP BY Org_Name, Fiscal_Year) AS b
                       ON  b.Org_Name = a.Org_Name  AND b.Fiscal_Year = a.Fiscal_Year")
+##Create "Legal Referral" column
+ActivityData <- sqldf("SELECT a.Org_Name, a.Fiscal_Year, a.Site_Visit, a.Knowledge_Exchange , a.Leveraging, a.Leveraging_Amount, a.Phone_Call, a.Email, a.Additional,b.Legal_Refer from ActivityData AS a LEFT JOIN 
+                      (SELECT Org_Name, Fiscal_Year, count(*) AS 'Legal_Refer' FROM MyData2 WHERE Capacity_Building_Type = 'Legal Referral' GROUP BY Org_Name, Fiscal_Year) AS b
+                      ON  b.Org_Name = a.Org_Name  AND b.Fiscal_Year = a.Fiscal_Year")
+
+
 
 stat.desc(ActivityData)
 
+##run a few data validation checks... took me a while to figure out some problems I had that led to me not counting certain things.-
+sum(MyData2$Capacity_Building_Type == 'Knowledge Exchange Participation')
+sum(ActivityData$Knowledge_Exchange, na.rm=TRUE)
+sum(is.na(ActivityData$Knowledge_Exchange) == FALSE)
+
+sum(MyData2$Capacity_Building_Type == 'Site Visit')
+sum(ActivityData$Site_Visit, na.rm=TRUE)
+sum(is.na(ActivityData$Site_Visit) == FALSE)
 
 
 
 
+## Next steps: join OCI_subset and ActivityData, then run LEAPS data on new combined dataframe to find correlations
 
-## Need to change the above line to put to a dataframe, then repeat the logic for all of the other activity types to build the new dataframe
+
+
 
